ccd0e072c96a4bbad906b082a71acaf73d89c6b0
Showrav <showravc85@gmail.com>
2026-04-17 19:54:52 +0600

    update message

--- first.js
+++ first.js
@@ -1,6 +1,10 @@
 console.log("Hello world");
 
 const os =require('os');
+const path=require('path');
 console.log(os.type())
 console.log(os.version())
 console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
