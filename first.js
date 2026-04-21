9d0eb22d6219e6f38a248a70ccb340229a5fd4d1
Showrav <showravc85@gmail.com>
2026-04-22 00:04:13 +0600

    update project

--- first.js
+++ first.js
@@ -1,20 +1,2 @@
-const http=require('node:http');
-http.createServer((request,response)=>{
-    const{headers,methods,url}=request;
-    let body=[];
-    request.on('error',err=>{
-        console.error(err);
-    })
-    .on('data',chunk=>{
-        body.push(chunk);
-    })
-    .on('end',()=>{
-body = Buffer.concat(body).toString();
-response.on(`error`,err=>{
-    console.error(err);
-});
-response.statusCode=200;
-response.setHeader('Content-type','application/json');
-const responseBody = { headers, method, url, body };
-    })
-})
+
+const math=require('./math');
