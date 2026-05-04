35cd2462078fbf39d4e6940d87909a2776876207
Showrav <showravc85@gmail.com>
2026-05-05 00:26:35 +0600

    message

--- first.js
+++ first.js
@@ -1,13 +1,16 @@
-const http=require("http");
-const fs=require("fs");
+const http = require("http");
+const fs = require("fs");
 
-const myServer =http.createServer((req,res)=>{
-const log='${Date.now()}:New Req Recived\n'
-  fs.appendFile('log.txt',log,(err,data))
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: New Req Received\n`;
+
+  fs.appendFile("log.txt", log, (err) => {
+    if (err) {
+      console.log("Error writing file:", err);
+    }
+  });
+
   res.end("Hello From server");
 });
 
-myServer.listen(8000,()=>console.log("Server Started!"));
-
-
-
+myServer.listen(8000, () => console.log("Server Started!"));
