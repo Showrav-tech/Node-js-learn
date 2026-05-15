2f46d9c75aec1add296b463cbceee6b74497b094
Showrav <showravc85@gmail.com>
2026-05-15 13:28:26 +0600

    continue

--- first.js
+++ first.js
@@ -7,4 +7,15 @@ const myserver = http.createServer((req,res)=>{
   const log=`${Date.now()}: ${req.url}New Req Received\n`;
   const myUrl=url.parse(req.url,true);
   console.log(myUrl);
+  fs.appendfile("log.text",log,(err,data)=>{
+    switch(myUrl.pathname){
+      case"/":
+      break;
+      case"/about":
+      break
+      const username=myUrl.query.myname;
+      res.end('Hi,${username}');
+      break;
+    }
+  })
 })
