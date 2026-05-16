103e7a85f220708eb3efcfd699fb8d827c6b3e26
Showrav <showravc85@gmail.com>
2026-05-16 20:02:37 +0600

    Continue

--- first.js
+++ first.js
@@ -1,34 +1,12 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express=require("express");
 
+
+const app=express();
 function myHandler(req,res){
-     if (req.url === "/favicon.ico") return res.end();
-
-    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.text", log, (err) => {
-        if (err) {
-            console.error("Log error:", err);
-        }
-        switch (myUrl.pathname) {
-         case "/":
-          res.end("Home Page");
-     break;
-            case "/about":
-         const username = myUrl.query.myname;
-                if (username) {
-                    res.end(`Hi, ${username}`);
-            } else {
-                    res.end("Hi, Guest");
-                }
-                break;
-
-            default:
-                res.end("404 Not Found");
-        }
-    });
+ 
 }
 
 const myserver = http.createServer=http.createServer(myHandler);
