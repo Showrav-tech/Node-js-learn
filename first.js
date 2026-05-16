daebe4896c21b35ebb246b67a9009117de7f5521
Showrav <showravc85@gmail.com>
2026-05-16 20:31:04 +0600

    why need express

--- first.js
+++ first.js
@@ -1,6 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+
 const express=require("express");
 
 
@@ -10,17 +9,10 @@ app.get('/',(req,res)=>{
     return res.send("Hello from Home page")
 });
 app.get('/about',(req,res)=>{
-    return res.send("Hello from about page")
+    return res.send(`Hello ${req.query.name}`);
 });
 
-function myHandler(req,res){
- 
-}
 
-const myserver = http.createServer(app);
+app.listen(8000,()=>console.log("Server started!"))
  
 
-myserver.listen(8000, () => {
-    console.log("Server Started Successfully!");
-    console.log("Go to: http://localhost:8000");
-});
