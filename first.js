81e77d08b9e10035834d46870d6692e6a6e63241
Showrav <showravc85@gmail.com>
2026-05-16 19:54:29 +0600

    Learn Express

--- first.js
+++ first.js
@@ -32,32 +32,7 @@ function myHandler(req,res){
 }
 
 const myserver = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") return res.end();
-
-    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.text", log, (err) => {
-        if (err) {
-            console.error("Log error:", err);
-        }
-        switch (myUrl.pathname) {
-            case "/":
-           res.end("Home Page");
-         break;
-            case "/about":
-            const username = myUrl.query.myname;
-                if (username) {
-                    res.end(`Hi, ${username}`);
-                } else {
-                    res.end("Hi, Guest");
-                }
-                break;
-
-            default:
-                res.end("404 Not Found");
-        }
-    });
+   
 });
 
 myserver.listen(8000, () => {
