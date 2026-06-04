b9d88bd87e238e728d6dd9ad3890dd574745079e
Showrav <showravc85@gmail.com>
2026-06-04 19:46:20 +0600

    REST API

--- first.js
+++ first.js
@@ -1,18 +1,41 @@
-const http = require("http");
-
-const express=require("express");
-
-
+const express =require("express");
+const users=require("./MOCK_DATA.json");
 const app=express();
-
-app.get('/',(req,res)=>{
-    return res.send("Hello from Home page")
+const PORT=8000;
+app.listen(PORT,()=> console.log(`Server Started at port :${PORT}`))
+app.get("/api/user",(req,res)=>{
+    return res.json(users);
 });
-app.get('/about',(req,res)=>{
-    return res.send(`Hello ${req.query.name}`);
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
 });
 
+//REST API
+app.get("/api/user/",(req,res)=>{
+    return res.json(users);
+});
 
-app.listen(8000,()=>console.log("Server started!"))
- 
+app.route("/api/users/:id").get("/api/users/:id",(req,res)=>{
+const id=Number(req.params.id);
+const user=users.find((user)=>user.id===id);
+return res.json(user);
+}).put((req,res)=>{
+    res.json({status:"Pending"});
+})
 
+app.post('/api/users',(req,res)=>{
+return res.json({status:"Pending"});
+}).delete((res,req)=>{
+    return res.json({status:"Pending"});
+});
+
+app.post("/api/users",(req,res)=>{
+    return res.json({status:"pending"});
+});
+
+app.listen(PORT,()=>console.log(`Server Started at PORT 8000`));
