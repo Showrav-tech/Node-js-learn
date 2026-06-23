31fc1d16bc722540af83609a759076efe0ff882b
Showrav <showravc85@gmail.com>
2026-06-23 21:13:02 +0600

    Meaningful commit message

--- first.js
+++ first.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const fs = require("fs"); 
+const mongoose=require("mongoose");
 const users = require("./MOCK_DATA.json");
 
 const app =express();
 const PORT = 8000;
+//Connection Db
+
+mongoose.connect(' mongodb://127.0.0.1:27017/youtube-app-1');
+
+//Schema
+const userSchema=new mongoose.Schema({
+firstName:{
+    type :String,
+    required:true,
+},
+lastName:{
+    type:String,
+
+},
+email:{
+    type:String,
+    required:true,
+    unique:true,
+},
+jobTitle:{
+    type:String,
+},gender:{
+    type:String,
+}
+});
+const User=mongoose.model("user",userSchema);
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -49,24 +76,15 @@ app.post("/api/users",(req,res)=>{
 
     if(
 
-        !body ||
-
-        !body.first_name||
-
-        !body.last_name||
-
-        !body.email||
-
-        !body.gender||
-
-        !body.job_title
+    !body ||
+    !body.first_name||
+     !body.last_name||
+      !body.email||
+    !body.gender||
+    !body.job_title
 
     ){
-
-        return res.status(400).json({msg:"All fields are req....."});
-
-
-
+     return res.status(400).json({msg:"All fields are req....."});
     }
 
     users.push({...body,id:users.length+1});
